13730f68df2b7dae52f972f4e667fb046cbd8ff6
ishikura keisuke <highchops1981@gmail.com>
2016-10-01 13:00:31 +0900

    change

---
+++ objective-c/LineUI1/LineUI1/SecondCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondCollectionView.swift
+//  LineUI1
+//
+//  Created by ishikura keisuke on 2016/09/29.
+//  Copyright © 2016年 ishikura keisuke. All rights reserved.
+//
+
+import Foundation
